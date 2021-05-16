4ab194097533bce1c6227244bc02ff27306b4ce4
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-05-16 16:41:55 -0700

    passed first test, starting on second test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+const firstName = "Danielle";
+const lastName = "Mathey";
+const address = "1234 Fake Street";
+const city = "Nowhere";
+const state = "Nevada";
+const zipcode = "12345";
 
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+
+  expect(screen.getByText(/Checkout Form/i));
+});
+
+test("form shows success message on submit with form details", async () => {});
